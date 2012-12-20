459152964b3e10096e274d590b42af5c70a9c2f3
Join.Gong <joinhack@gmail.com>
2012-12-20 18:29:53 +0800

    test

--- b-tree/btree.c
+++ b-tree/btree.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
 
-bnode search(bnode *node) {
-	bnode *current = node, *max, *min;
-	while(current) {
 
-		while(max >= min) {
-			
+bnode* bnodeInsertNonfull(bnode *node, int val);
+
+bnode bnodeSearch(bnode *node, int val);
+
+static bnode bnodeSearch(bnode *node, int val) {
+	bnode *current = node;
+	size_t max, min, i;
+	while(current) {
+		i = 0;
+		while(i < current->size && val > current->vals[i]) {
+			i++;
 		}
+		if(val == current->vals[i]) {
+			return current;
+		}
+		if(current->leaf)
+			return NULL;
+		current = current->children[i];
+	}
+}
+
+bnode* btreeSearch(btree *tree, int val) {
+	return bnodeSearch(tree->root, val);
+}
+
+static bnode* bnodeInsertNonfull(bnode *node, int val) {
+	int i = node->size - 1;
+	if(node.leaf) {
+		while(i >= 0 && val < node->vals[i]) {
+			node->vals[i+1] = node->vals[i];
+			i--;
+		}
+		node->vals[i] = val;
+	}
+}
+
+bnode* btreeInsert(btree *tree, int val) {
+	bnode *node = tree->node;
+	if(node->size == tree.arraySize) {
+
+	} else {
+		bnodeInsertNonfull(node, val);
 	}
 }
 
--- b-tree/btree.h
+++ b-tree/btree.h
@@ -2,13 +2,19 @@
 #define _BTREE_
 
 typedef struct _bnode {
-	int *val;
+	int *vals;
+	int leaf;
 	int size;	// val array size.
 	struct _bnode *children;	//the children array size should be size + 1
 } bnode;
 
 typedef struct _btree {
-	
+	bnode *root;
+	size_t arraySize;
 } btree;
+
+bnode* btreeSearch(btree *tree, int val);
+
+bnode* btreeInsert(btree *tree, int val);
 
 #endif
