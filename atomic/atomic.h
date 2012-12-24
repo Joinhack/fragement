4ad4b5fec21f0f3580a4dbd710595382ebea6292
Join.Gong <joinhack@gmail.com>
2012-12-24 18:56:00 +0800

    test

--- atomic/atomic.h
+++ atomic/atomic.h
@@ -4,13 +4,39 @@
 #ifndef _INLINE
 #define _INLINE __inline__
 
-uint64_t	atomic_add_uint64(uint64_t *p, uint64_t x);
-uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t x);
+uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v);
+uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v);
 
-uint64_t	atomic_add_uint32(uint64_t *p, uint32_t x);
-uint64_t	atomic_sub_uint32(uint64_t *p, uint32_t x);
+uint64_t	atomic_add_uint32(uint64_t *p, uint32_t v);
+uint64_t	atomic_sub_uint32(uint64_t *p, uint32_t v);
 
+#ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+_INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
+	return __sync_add_and_fetch(p, v);
+}
 
+_INLINE uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v) {
+	return __sync_sub_and_fetch(p, v);
+}
+#elif (defined(__amd64_) || defined(__x86_64__))
+_INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
+	asm volatile (
+		"lock; xaddq %0, %1;"
+		: "+r" (v), "=m" (*p)
+		: "m" (*p)
+		);
+	return (v);
+}
+_INLINE uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v) {
+	v = -v;
+	asm volatile (
+		"lock; xaddq %0, %1;"
+		: "+r" (v), "=m" (*p)
+		: "m" (*p)
+		);
+	return (v);
+}
+#endif
 
 
 #endif /*ATOMIC head define**/
