f50722718f7c24d9e2a3e5ce36641c720dc4e805
Join.Gong <joinhack@gmail.com>
2012-12-27 19:07:57 +0800

    test

--- chart/cevent.c
+++ chart/cevent.c
@@ -25,3 +25,29 @@ void destory_cevents(cevents *cevts) {
 	cevts->fired = NULL;
 	jfree(cevts);
 }
+
+void cevents_add_event(cevents *cevts, int fd, int mask, event_proc *proc) {
+	if(fd > MAX_EVENTS)
+		return -1; //TODO: change to error code
+	cevent *evt = &cevts->events[fd];
+	if(mask & CEV_READ) evt->read = proc;
+	if(mask & CEV_WRITE) evt->write = proc;
+	evt->mask |= mask;
+}
+
+void cevents_del_event(cevents *cevts, int fd, int mask) {
+	size_t j;
+	if(fd > MAX_EVENTS)
+		return -1; //TODO: change to error code
+	cevent *evt = &cevts->events[fd];
+	if(mask & CEV_READ) evt->read = NULL;
+	if(mask & CEV_WRITE) evt->write = NULL;
+	evt->mask &= ~mask; //remove mask
+	//change maxfd
+	if(cevts->maxfd && evt->mask == CEV_NONE) {
+		for(j = cevts->maxfd - 1; j>= 0; j--) {
+			if(cevts->events[j].mask != CEV_NONE)
+				cevts.maxfd = j;
+		}
+	}
+}
