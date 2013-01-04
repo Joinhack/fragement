2f09117b3d3e712c1f5d0cadccb7842759bf6757
Join.Gong <joinhack@gmail.com>
2013-01-04 12:23:51 +0800

    test

--- chart/atomic.h
+++ chart/atomic.h
@@ -14,6 +14,7 @@ uint32_t	atomic_sub_uint32(uint32_t *p, uint32_t v);
 int atomic_cmp_and_swap_uint32(uint32_t *p, uint32_t o, uint32_t n);
 
 #ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+
 INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
 	return __sync_add_and_fetch(p, v);
 }
@@ -21,10 +22,13 @@ INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
 INLINE uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v) {
 	return __sync_sub_and_fetch(p, v);
 }
+
 INLINE int atomic_cmp_and_swap_uint64(uint64_t *p, uint64_t o, uint64_t n) {
 	return __sync_bool_compare_and_swap(p, o, n);
 }
+
 #elif (defined(__amd64_) || defined(__x86_64__))
+
 INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
 	asm volatile (
 		"lock; xaddq %0, %1;"
@@ -33,6 +37,7 @@ INLINE uint64_t	atomic_add_uint64(uint64_t *p, uint64_t v) {
 		);
 	return (*p);
 }
+
 INLINE uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v) {
 	v = -v;
 	asm volatile (
@@ -42,6 +47,7 @@ INLINE uint64_t	atomic_sub_uint64(uint64_t *p, uint64_t v) {
 		);
 	return (*p);
 }
+
 INLINE int atomic_cmp_and_swap_uint64(uint64_t *p, uint64_t o, uint64_t n) {
 	char result;
 	asm volatile (
@@ -51,6 +57,7 @@ INLINE int atomic_cmp_and_swap_uint64(uint64_t *p, uint64_t o, uint64_t n) {
 		);
 	return (result);
 }
+
 #else
 #  error "not support atomic"
 #endif  //end define the 64 bit
@@ -58,16 +65,21 @@ INLINE int atomic_cmp_and_swap_uint64(uint64_t *p, uint64_t o, uint64_t n) {
 
 
 #ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+
 INLINE uint32_t	atomic_add_uint32(uint32_t *p, uint32_t v) {
 	return __sync_add_and_fetch(p, v);
 }
+
 INLINE uint32_t	atomic_sub_uint32(uint32_t *p, uint32_t v) {
 	return __sync_sub_and_fetch(p, v);
 }
+
 INLINE int atomic_cmp_and_swap_uint32(uint32_t *p, uint32_t o, uint32_t n) {
 	return __sync_bool_compare_and_swap(p, o, n);
 }
+
 #elif (defined(__i386__) || defined(__amd64_) || defined(__x86_64__))
+
 INLINE uint32_t	atomic_add_uint32(uint32_t *p, uint32_t v) {
 	asm volatile (
 		"lock; xaddl %0, %1;"
@@ -76,6 +88,7 @@ INLINE uint32_t	atomic_add_uint32(uint32_t *p, uint32_t v) {
 		);
 	return (*p);
 }
+
 INLINE uint32_t	atomic_sub_uint32(uint32_t *p, uint32_t v) {
 	v = -v;
 	asm volatile (
@@ -85,6 +98,7 @@ INLINE uint32_t	atomic_sub_uint32(uint32_t *p, uint32_t v) {
 		);
 	return (*p);
 }
+
 INLINE int atomic_cmp_and_swap_uint32(uint32_t *p, uint32_t o, uint32_t n) {
 	char result;
 	asm volatile (
@@ -94,6 +108,7 @@ INLINE int atomic_cmp_and_swap_uint32(uint32_t *p, uint32_t o, uint32_t n) {
 		);
 	return (result);
 }
+
 #else
 #  error "not support atomic"
 #endif
