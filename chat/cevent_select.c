cb9e69dc9a84601e1e16090451f768271948256b
Join.Gong <joinhack@gmail.com>
2013-01-05 17:05:00 +0800

    test

---
+++ chat/cevent_select.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <sys/select.h>
+#include "jmalloc.h"
+#include "cevent.h"
+
+typedef struct {
+	fd_set wfds;
+	fd_set rfds;
+} rwfd_set;
+
+static int cevents_create_priv_impl(cevents *cevts) {
+	rwfd_set *rwfds = jmalloc(sizeof(rwfd_set));
+	FD_ZERO(&rwfds->rfds);
+	FD_ZERO(&rwfds->wfds);
+	cevts->priv_data = rwfds;
+	return 0;
+}
+
+static int cevents_destory_priv_impl(cevents *cevts) {
+	jfree(cevts->priv_data);
+	return 0;
+}
+
+static int cevents_add_event_impl(cevents *cevts, int fd, int mask) {
+	rwfd_set *rwfds = (rwfd_set*)cevts->priv_data;
+	if(mask & CEV_READ) FD_SET(fd, &rwfds->rfds);
+	if(mask & CEV_WRITE) FD_SET(fd, &rwfds->wfds);
+	return 0;
+}
+
+static int cevents_del_event_impl(cevents *cevts, int fd, int mask) {
+	rwfd_set *rwfds = (rwfd_set*)cevts->priv_data;
+	if(mask & CEV_READ) FD_CLR(fd, &rwfds->rfds);
+	if(mask & CEV_WRITE) FD_CLR(fd, &rwfds->wfds);
+	return 0;
+}
+
+static int cevents_poll_impl(cevents *cevts, msec_t ms) {
+	rwfd_set *rwfds = (rwfd_set*)cevts->priv_data;
+	fd_set work_rfds, work_wfds;
+	cevent *event;
+	cevent_fired *fired;
+	int rs, i;
+	int mask = 0;
+	int count = 0;
+	struct timeval tv;
+
+	tv.tv_sec = (long) (ms / 1000);
+  tv.tv_usec = (long) ((ms % 1000) * 1000);
+
+	//copy the read write fd_set, if use original, will have problem.
+	work_rfds = rwfds->rfds;
+	work_wfds = rwfds->wfds;
+	rs = select(cevts->maxfd, &work_rfds, &work_wfds, NULL, &tv);
+
+	if(rs > 0) {
+		for(i = 0; i <= cevts->maxfd; i++) {
+			mask = CEV_NONE;
+			event = &cevts->events[i];
+			if(event->mask == CEV_NONE)
+				continue;
+			if((event->mask & CEV_WRITE) && FD_ISSET(i, &work_wfds))
+				mask |= CEV_WRITE;
+			if((event->mask & CEV_READ) && FD_ISSET(i, &work_rfds))
+				mask |= CEV_READ;
+			fired = jmalloc(sizeof(cevent_fired));
+			fired->fd = i;
+			fired->mask = mask;
+			cevents_push_fired(cevts, fired);
+			count++;
+		}
+	}
+	return count;
+}
