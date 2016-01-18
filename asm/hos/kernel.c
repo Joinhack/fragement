0c403967384e468547fc5a0d6c718370e44e3c50
joinhack <joinhack@gmail.com>
2016-01-18 14:33:25 +0800

    pm rivt

--- asm/hos/kernel.c
+++ asm/hos/kernel.c
@@ -11,6 +11,22 @@ void reinstall_idt() {
 	asm volatile("lidtl %0\n" : : "m" (idt_tables));
 }
 
+void reinstall_gdt() {
+	//use the same code segment and data segment define in main.
+	gdt[1] = ((struct desc_struct)GDT_ENTRY_INIT(0xc09b, 0, 0xfffff));
+	gdt[2] = ((struct desc_struct)GDT_ENTRY_INIT(0xc093, 0, 0xfffff));
+	
+	static const struct gdt_ptr gdt_tables = {sizeof(struct desc_struct), (u32)gdt};
+	asm volatile(
+		"lgdtl %0\n"
+		"movl $(2*8), %%ecx\n"
+		"movl %%ecx, %%ds\n"
+		"movl %%ecx, %%ds\n"
+		"movl %%ecx, %%es\n"
+		"movl %%ecx, %%fs\n"
+		"movl %%ecx, %%gs\n"
+		:: "m" (gdt_tables));
+}
 
 void __entry kentry() {
 	reinstall_idt();
